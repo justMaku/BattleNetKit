09d53e8353d87bd25e9e6869a60b7355aa85db9d
Michał Kałużny <maku@justmaku.org>
2017-12-05 20:07:13 +0100

    Cleaned up the project a little bit.

--- generate_protos.sh
+++ generate_protos.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-protoc --swift_out=Sources/BattleNetKit/Models --proto_path=Proto/ Proto/*.proto 
+protoc --swift_out=Sources/BattleNetKit/Packets/Aurora --proto_path=Proto/ Proto/*.proto 
