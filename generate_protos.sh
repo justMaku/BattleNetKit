b49415cdc5c68b4143d25873edb0e9b623ff4475
Michał Kałużny <maku@justmaku.org>
2019-08-22 17:35:09 +0200

    Generate a flat hierarchy of protobuf definitions to make swift compiler happy since it requires every filename to be unique.

--- generate_protos.sh
+++ generate_protos.sh
@@ -1,5 +1,9 @@
 output_dir="Sources/BattleNetKit/Packets/Aurora"
+
+rm -rf "${output_dir}"
+mkdir "${output_dir}"
+
 find ./Protocol -name '*.proto' -print0 | xargs -0  \
-  protoc --swift_out="${output_dir}"       \
+  protoc --swift_out=FileNaming=PathToUnderscores:"${output_dir}"       \
          --swift_opt=Visibility=Public     \
          --proto_path=Protocol/
