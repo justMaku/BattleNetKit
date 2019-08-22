0d2517cfe16e3ec1bcb0602fec6fabb874d98446
Michał Kałużny <maku@justmaku.org>
2019-08-22 17:36:15 +0200

    Update all the dependencies.

--- Package.swift
+++ Package.swift
@@ -17,11 +17,11 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.0.0"),
-        .package(url: "https://github.com/onevcat/Rainbow", from: "3.1.5"),
-        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.5.0"),
         .package(url: "https://github.com/IBM-Swift/CZlib", from: "0.1.2"),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.3.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.6.0"),
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
+        .package(url: "https://github.com/onevcat/Rainbow", from: "3.1.5"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
