6d3c6d31abb7a2b9b3d9b3fc5796140328284b01
Michał Kałużny <maku@justmaku.org>
2020-01-23 14:56:48 -0800

    Expose few of the `Account` service methods through AccountAPI

--- Sources/BattleNetKit/APIs/AccountAPI.swift
+++ Sources/BattleNetKit/APIs/AccountAPI.swift
@@ -26,6 +26,48 @@ public class AccountAPI: API {
         try serviceProvider.register(inbound: self.clientService)
         try serviceProvider.register(outbound: self.serverService)
     }
+
+    public func getAccountState(account: Bgs_Protocol_EntityId) -> EventLoopFuture<Bgs_Protocol_Account_V1_AccountState> {
+        var request = Bgs_Protocol_Account_V1_GetAccountStateRequest()
+
+        request.entityID = account
+
+        return self.serverService.GetAccountState(request: request).map { $0.state }
+    }
+
+    public func getGameAccountState(
+        accountID: Bgs_Protocol_EntityId,
+        gameAccountID: Bgs_Protocol_EntityId
+    ) -> EventLoopFuture<Bgs_Protocol_Account_V1_GameAccountState> {
+        var request = Bgs_Protocol_Account_V1_GetGameAccountStateRequest()
+
+        request.accountID = accountID
+        request.gameAccountID = gameAccountID
+        request.options = .init()
+        request.options.allFields = true
+
+        return self.serverService
+            .GetGameAccountState(request: request).map { $0.state }
+    }
+
+    public func getAuthorizedData(accountID: Bgs_Protocol_EntityId) -> EventLoopFuture<Bgs_Protocol_Account_V1_GetAuthorizedDataResponse> {
+        var request = Bgs_Protocol_Account_V1_GetAuthorizedDataRequest()
+        request.entityID = accountID
+
+        return self.serverService.GetAuthorizedData(request: request)
+    }
+
+    public func getLicenses(accountID: Bgs_Protocol_EntityId) -> EventLoopFuture<Bgs_Protocol_Account_V1_GetLicensesResponse> {
+        var request = Bgs_Protocol_Account_V1_GetLicensesRequest()
+
+        request.excludeUnknownProgram = false
+        request.fetchAccountLicenses = true
+        request.fetchGameAccountLicenses = true
+        request.fetchDynamicAccountLicenses = true
+        request.targetID = accountID
+
+        return self.serverService.GetLicenses(request: request)
+    }
 }
 
 extension AccountAPI: Bnet_Protocol_Account_AccountNotifyHandler {
