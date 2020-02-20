7cf3ed4cf4e1fb4c1dd476d84c50a5050445e28a
Michał Kałużny <maku@justmaku.org>
2020-02-20 16:53:36 +0100

    Update to latest version of dependencies.

--- Package.swift
+++ Package.swift
@@ -20,9 +20,9 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/IBM-Swift/CZlib", from: "0.1.2"),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.3.0"),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.6.0"),
-        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.6.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.14.0"),
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.8.0"),
         .package(url: "https://github.com/onevcat/Rainbow", from: "3.1.5"),
         .package(url: "https://github.com/jakeheis/SwiftCLI", from: "6.0.0"),
     ],
